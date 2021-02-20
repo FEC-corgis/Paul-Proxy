4b5cacae24c46766736f0617cb50d47849f3d739
Ubuntu <ubuntu@ip-172-31-23-82.us-west-1.compute.internal>
2021-02-20 02:33:47 +0000

    Deployed with all services rendered

--- server/index.js
+++ server/index.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const path = require('path');
 const {createProxyMiddleware} = require('http-proxy-middleware');
+const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
-app.use('/rooms/:id', express.static(path.join(__dirname, '../client')));
+app.use(cors());
 
 // paul
-app.use('/propertyDetails/:id', createProxyMiddleware({target: 'http://localhost:5545', changeOrigin:true}));
+app.use('/propertyDetails/:pId', createProxyMiddleware({ target: 'http://54.176.104.176:5545', changeOrigin: true }))
 
 // jenny
-app.use('/reviews/propId/:id', createProxyMiddleware({ target: 'http://localhost:1984', changeOrigin: true }))
-app.use('/morePlaces/propId/:id', createProxyMiddleware({ target: 'http://localhost:1985', changeOrigin: true }))
+app.use('/reviews/propId/:id', createProxyMiddleware({ target: 'http://3.22.194.10:1984/', changeOrigin: true }))
+app.use('/morePlaces/propId/:id', createProxyMiddleware({ target: 'http://18.222.249.216:1985/', changeOrigin: true }))
 
 //dane
-app.use('/api/headerService/:id', createProxyMiddleware({ target: 'http://localhost:5001', changeOrigin: true }))
+app.use('/api/headerService/:propertyId', createProxyMiddleware({ target: 'http://ec2-34-228-69-178.compute-1.amazonaws.com:5001', changeOrigin: true }))
+app.use('/api/hostedByService/:propertyId', createProxyMiddleware({ target: 'http://ec2-18-207-211-57.compute-1.amazonaws.com:5002', changeOrigin: true }))
 
-//slava
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+});
 
 app.listen(port, ()=>console.log('Proxy server listening on port '+ port));
